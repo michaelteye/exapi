abeb3d31680dd85b774bbe623ac983b11fed9edc
michaelteye <michaelteyegyemah@gmail.com>
2022-11-17 17:25:47 +0000

    new change

--- src/api/player/getList.js
+++ src/api/player/getList.js
@@ -4,27 +4,8 @@
 
 import Player from "../../db/model/player";
 
-// export default async (req, res) => {
-//   res.sendStatus(500);
-// }
+export default async (req, res) => {
+  res.sendStatus(500);
+}
 
 
-app.get( "/all",async (req,res)=>{
- 
-  res.sendStatus(200);
- 
-})
-
-// // adey come wai. adey search the error for ma machine top
-// export default async ('/:player',async (req,res) => {
-//   try{
-//     const {player} = req.params
-   
-//     const currentPlayer = await Player.findByPk(player);
-//     return res.status(200).send(currentPlayer)
-
-// }
-// catch(err){
-// res.sendStatus(500);
-// }
-// })
