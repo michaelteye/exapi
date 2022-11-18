fc3991781c54b905da9e983cfffe9055aef8060c
BernardKwesi <bernarddormon31@gmail.com>
2022-11-18 13:18:47 +0000

    Implement player create functionality

--- src/api/player/create.js
+++ src/api/player/create.js
@@ -2,6 +2,36 @@
 // YOU CAN FREELY MODIFY THE CODE BELOW IN ORDER TO COMPLETE THE TASK
 // ---------------------------------------------------------------------------------------------
 
+import Player from "../../db/model/player";
+import PlayerSkill from "../../db/model/playerSkill";
+
+const PlayerSkills = Player.hasMany(PlayerSkill, { as: "playerSkills" });
+
 export default async (req, res) => {
-  res.sendStatus(500);
-}
+  try {
+    //Bernard remember to validate the request
+    const player = await Player.create(
+      {
+        name: req.body.name,
+        position: req.body.position,
+        playerSkills: req.body.playerSkills,
+      },
+      {
+        include: [
+          {
+            association: PlayerSkills,
+            as: "playerSkills",
+          },
+        ],
+      }
+    );
+
+    let savedPlayer = await Player.findAll({
+      include: { model: PlayerSkill, as: "playerSkills" },
+    });
+
+    res.json(savedPlayer);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
