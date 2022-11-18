eb5b857d090fa25d6c9156b53ee09e23d4f6ed9d
BernardKwesi <bernarddormon31@gmail.com>
2022-11-18 20:44:20 +0000

    Implement delete functionality

--- src/api/player/update.js
+++ src/api/player/update.js
@@ -12,56 +12,36 @@ export default async (req, res) => {
   //get the player id
   const playerId = req.params.id;
 
-  let updatePlayer = Player.findOne({
-    where: { id: playerId },
-    include: [
-      {
+  // check if the id exists in db
+  let player = await Player.findOne({ where: { id: playerId } });
+
+  if (player == null) {
+    return res.status(404).json({ message: "Player does not exist" });
+  }
+
+  await Player.update(
+    {
+      name: req.body.name,
+      position: req.body.position,
+      playerSkills: req.body.playerSkills,
+    },
+    {
+      where: { id: playerId },
+    },
+    {
+      include: {
         model: PlayerSkill,
-        as: "playerSkills",
+        as: "updatePlayerSkills",
       },
-    ],
-  }).then((player) => {
-    if (!player)
-      return res.status(404).json({ message: "Player does not exist" });
+    }
+  );
 
-    //return res.json(player);
-    player.playerSkills.skill = "Hello, world";
-    player.playerSkills.value = 80;
-    player.save();
-
-    return res.json(player); //or res.json('ok updated');
+  let updatedPlayer = await Player.findOne({
+    where: { id: player.id },
+    include: { model: PlayerSkill, as: "playerSkills" },
   });
 
-  //check if the id exists in db
-  // let player = await Player.findOne({ where: { id: playerId } });
-
-  // if (player == null) {
-  //   return res.status(404).json({ message: "Player does not exist" });
-  // }
-
-  // let updatePlayer = await Player.update(
-  //   {
-  //     name: req.body.name,
-  //     position: req.body.position,
-  //     playerSkills: req.body.playerSkills,
-  //   },
-  //   {
-  //     where: { id: playerId },
-  //   },
-  //   {
-  //     include: {
-  //       association: UpdatePlayerSkills,
-  //       as: "updatePlayerSkills",
-  //     },
-  //   }
-  // );
-
-  // let updatedPlayer = await Player.findOne({
-  //   where: { id: player.id },
-  //   include: { model: PlayerSkill, as: "playerSkills" },
-  // });
-
-  //  res.json(updatedPlayer);
+  res.json(updatedPlayer);
   /* } catch (error) {
     res.status(500).send(error);
   } */
