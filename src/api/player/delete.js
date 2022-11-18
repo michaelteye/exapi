825185edb8d37355d37b0cdb879945aa7d492e55
BernardKwesi <bernarddormon31@gmail.com>
2022-11-18 18:13:33 +0000

    Check for authorization token before delete is performed

--- src/api/player/delete.js
+++ src/api/player/delete.js
@@ -3,5 +3,21 @@
 // ---------------------------------------------------------------------------------------------
 
 export default async (req, res) => {
-  res.sendStatus(500);
-}
+  const token =
+    "SkFabTZibXE1aE14ckpQUUxHc2dnQ2RzdlFRTTM2NFE2cGI4d3RQNjZmdEFITmdBQkE=";
+  const bearerHeader = req.headers["authorization"];
+  console.log(req.headers);
+  if (typeof bearerHeader == "undefined") {
+    return res
+      .status(403)
+      .json({ message: "You are not authorized to perfrom this action." });
+  }
+  const bearer = bearerHeader.split(" ");
+  const bearerToken = bearer[1];
+  if (bearerToken !== token) {
+    return res
+      .status(403)
+      .json({ message: "Invalid Authorization token supplied." });
+  }
+  return res.json({ message: "Player Deleted" });
+};
