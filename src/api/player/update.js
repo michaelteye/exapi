5153558cf07abb38140d44afe765a5a35fb26551
BernardKwesi <bernarddormon31@gmail.com>
2022-11-19 09:02:00 +0000

    Implemented Update functionality

--- src/api/player/update.js
+++ src/api/player/update.js
@@ -4,45 +4,56 @@
 
 import Player from "../../db/model/player";
 import PlayerSkill from "../../db/model/playerSkill";
+import database from "../index";
 
 export default async (req, res) => {
-  /* try { */
-  //Bernard remember to validate the request
+  try {
+    //Bernard remember to validate the request
 
-  //get the player id
-  const playerId = req.params.id;
+    //get the player id
+    const playerId = req.params.id;
 
-  // check if the id exists in db
-  let player = await Player.findOne({ where: { id: playerId } });
+    // check if the id exists in db
+    let player = await Player.findOne({ where: { id: playerId } });
 
-  if (player == null) {
-    return res.status(404).json({ message: "Player does not exist" });
-  }
-
-  await Player.update(
-    {
-      name: req.body.name,
-      position: req.body.position,
-      playerSkills: req.body.playerSkills,
-    },
-    {
-      where: { id: playerId },
-    },
-    {
-      include: {
-        model: PlayerSkill,
-        as: "updatePlayerSkills",
-      },
+    if (player == null) {
+      return res.status(404).json({ message: "Player does not exist" });
     }
-  );
+    //update the player details with the incoming player data
+    await player.update(
+      {
+        name: req.body.name,
+        position: req.body.position,
+      },
+      {
+        where: { id: playerId },
+      }
+    );
+    //delete the already skills associated with this player
+    await PlayerSkill.destroy({
+      where: { playerId: player.id },
+    });
+    //get the player skills from the request body
+    let playerSkills = req.body.playerSkills;
 
-  let updatedPlayer = await Player.findOne({
-    where: { id: player.id },
-    include: { model: PlayerSkill, as: "playerSkills" },
-  });
+    //attach the new skills to the player
+    playerSkills.forEach(async (playerSkill) => {
+      await PlayerSkill.create({
+        skill: playerSkill.skill,
+        value: playerSkill.value,
+        playerId: player.id,
+      });
+    });
 
-  res.json(updatedPlayer);
-  /* } catch (error) {
-    res.status(500).send(error);
-  } */
+    let updatedPlayer = await Player.findOne({
+      where: { id: player.id },
+      include: { model: PlayerSkill, as: "playerSkills" },
+    });
+
+    return res.json(updatedPlayer);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error });
+  }
 };
