81562127deb3a7d2cbd089db6958e24f2e577ef2
BernardKwesi <bernarddormon31@gmail.com>
2022-11-20 23:20:12 +0000

    Implementation not done

--- src/api/team/process.js
+++ src/api/team/process.js
@@ -2,6 +2,31 @@
 // YOU CAN FREELY MODIFY THE CODE BELOW IN ORDER TO COMPLETE THE TASK
 // ---------------------------------------------------------------------------------------------
 
+import Player from "../../db/model/player";
+import PlayerSkill from "../../db/model/playerSkill";
+const PlayerSkills = Player.hasMany(PlayerSkill, { as: "playerSkill" });
+
 export default async (req, res) => {
-  res.sendStatus(500);
-}
+  let teamRequests = req.body;
+
+  await teamRequests.forEach(async (teamRequest) => {
+    let player = await Player.findAll({
+      where: { position: teamRequest.position },
+      limit: teamRequest.numberOfPlayers,
+      attributes: ["name", "position"],
+      include: {
+        model: PlayerSkill,
+        as: "playerSkills",
+        attributes: ["skill", "value"],
+        where: {
+          skill: teamRequest.mainSkill,
+        },
+      },
+    });
+
+    res.json(player);
+    //return res.json(player);
+  });
+
+  return;
+};
